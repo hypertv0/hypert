4215d8434b73b3b6e550914e72c5c6820740b7c5
hypertv0 <hypertvcors0@gmail.com>
2025-11-30 20:39:25 +0300

    Create build.gradle.kts

---
+++ plugin/build.gradle.kts
@@ -0,0 +1,22 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
+    }
+}
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+task("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
