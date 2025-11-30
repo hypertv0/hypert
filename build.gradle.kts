9702d84d25da21f49d4b8367cd4599493b69ebaa
hypertv0 <hypertvcors0@gmail.com>
2025-11-30 19:58:00 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,11 @@ buildscript {
         maven("https://jitpack.io")
     }
     dependencies {
+        // Android ve Kotlin araçları
         classpath("com.android.tools.build:gradle:7.4.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
+        // CloudStream Eklenti Aracı (Bunu elle ekledik ki hata vermesin)
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
     }
 }
 
