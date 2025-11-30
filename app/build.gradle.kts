e45197746ac2205416df391bc9a402b9b5bded86
hypertv0 <hypertvcors0@gmail.com>
2025-11-30 19:44:47 +0300

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("com.github.recloudstream.gradle") version "master-SNAPSHOT"
+}
+
+cloudstream {
+    name = "HyperT"
+    label = "HyperT"
+    description = "trgoal"
+    authors = "HyperTV0"
+    version = 1
+}
+
+android {
+    compileSdk = 33
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 33
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation("com.github.recloudstream:cloudstream:pre-release")
+    implementation(kotlin("stdlib"))
+    implementation("com.github.Blatzar:NiceHttp:0.4.1")
+    implementation("org.jsoup:jsoup:1.13.1")
+}
