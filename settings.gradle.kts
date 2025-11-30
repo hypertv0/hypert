adc0f5e2097b4412a67a453b86d17545f268f02a
hypertv0 <hypertvcors0@gmail.com>
2025-11-30 19:59:03 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,13 @@ pluginManagement {
         google()
         maven("https://jitpack.io")
     }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "com.github.recloudstream.gradle") {
+                useModule("com.github.recloudstream:gradle:master-SNAPSHOT")
+            }
+        }
+    }
 }
 
 rootProject.name = "HyperT"
